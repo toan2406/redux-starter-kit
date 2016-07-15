a0558be3575f4612cba0b11b954fb1f3bbe36e6f
Toan Nguyen <toan2406@gmail.com>
2016-07-15 14:30:09 +0700

    add requiresAuth component

--- server/routes.js
+++ server/routes.js
@@ -4,13 +4,16 @@ import Home from '../shared/containers/Home'
 import Login from '../shared/containers/Login'
 import Post from '../shared/containers/Post'
 import About from '../shared/containers/About'
+import requiresAuth from '../shared/higher-order-components/requiresAuth'
+import ProtectedPage from '../shared/containers/ProtectedPage'
 
 const indexRoute = { component: Home }
 const childRoutes = [
   { component: Shell, path: '/shell' },
   { component: Login, path: '/login' },
   { component: Post, path: '/posts/:postId' },
-  { component: About, path: '/about' }
+  { component: About, path: '/about' },
+  { component: requiresAuth(ProtectedPage), path: '/protected' }
 ]
 
 export default {
